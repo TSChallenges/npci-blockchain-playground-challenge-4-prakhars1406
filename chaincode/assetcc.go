3479ddf32023665be29c1e5473146bf9b2efa26d
Prakhar Sharma <prakhars1406@gmail.com>
2025-02-23 15:01:53 +0000

    pushing playground challenge 4 solution

--- chaincode/assetcc.go
+++ chaincode/assetcc.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"time"
 )
 
 type Asset struct {
@@ -18,30 +16,305 @@ type Asset struct {
 }
 
 type Investor struct {
-	InvestorID string `json:"investor_id"`
-	Balance    int    `json:"balance"`
+	InvestorID             string           `json:"investor_id"`
+	Balance                int              `json:"balance"`
+	Holdings               map[string]int   `json:"holdings"`
+	SubscriptionTimestamps map[string]int64 `json:"subscriptionTimestamps"`
 }
 
 type AssetManagementContract struct {
 	contractapi.Contract
 }
 
-// TODO: CreateUser function adds an investor to the ledger
+var (
+	LockInTime int64 = 7 * 24 * 60 * 60 // 7 days in seconds
+)
 
+type SubscriptionRedeemRequest struct {
+	InvestorID string `json:"investorID"`
+	ISIN       string `json:"isin"`
+	Units      int    `json:"units"`
+	Timestamp  int64  `json:"timestamp"`
+}
+
+// TODO: CreateUser function adds an investor to the ledger
+func (amc *AssetManagementContract) CreateUser(ctx contractapi.TransactionContextInterface, investor Investor) error {
+	if investor.InvestorID == "" {
+		fmt.Errorf("investor id cannot be empty")
+		return fmt.Errorf("investor id cannot be empty")
+	}
+
+	if investor.Balance <= 0 {
+		fmt.Errorf("investor balance cannot be less then 0")
+		return fmt.Errorf("investor balance cannot be less then 0")
+	}
+
+	data, err := ctx.GetStub().GetState(investor.InvestorID)
+	if err == nil || data != nil {
+		fmt.Errorf("investor already exists")
+		return fmt.Errorf("investor already exists")
+	}
+
+	investorDataInBytes, err := json.Marshal(investor)
+	if err != nil {
+		fmt.Errorf("error in marshalling investor data  %s", err.Error())
+		return fmt.Errorf("error in marshalling investor data")
+	}
+
+	err = ctx.GetStub().PutState(investor.InvestorID, investorDataInBytes)
+	if err != nil {
+		fmt.Errorf("error in adding investor %s", err.Error())
+		return fmt.Errorf("error in adding investor")
+	}
+
+	err = ctx.GetStub().SetEvent("CreateUser", investorDataInBytes)
+	if err != nil {
+		fmt.Errorf("error in setting event %s", err.Error())
+		return err
+	}
+
+	return nil
+}
 
 // TODO: RegisterAsset function registers a new asset
 
+func (amc *AssetManagementContract) RegisterAsset(ctx contractapi.TransactionContextInterface, asset Asset) error {
+	if asset.ISIN == "" {
+		fmt.Errorf("asset ISIN cannot be empty")
+		return fmt.Errorf("asset ISIN cannot be empty")
+	}
 
-// TODO: SubscribeAsset function allows an investor to subscribe to asset units
+	if asset.CompanyName == "" {
+		fmt.Errorf("asset CompanyName cannot be empty")
+		return fmt.Errorf("asset CompanyName cannot be empty")
+	}
 
+	if asset.AssetType == "" {
+		fmt.Errorf("asset AssetType cannot be empty")
+		return fmt.Errorf("asset AssetType cannot be empty")
+	}
 
-// TODO: RedeemAsset function allows an investor to redeem asset units
+	if asset.TotalUnits <= 0 {
+		fmt.Errorf("asset TotalUnits cannot be less then 0")
+		return fmt.Errorf("asset TotalUnits cannot be less then 0")
+	}
 
+	if asset.PricePerUnit <= 0 {
+		fmt.Errorf("asset PricePerUnit cannot be less then 0")
+		return fmt.Errorf("asset PricePerUnit cannot be less then 0")
+	}
 
-// TODO: GetPortfolio function retrieves an investor's portfolio
+	if asset.AvailableUnits <= 0 {
+		fmt.Errorf("asset AvailableUnits cannot be less then 0")
+		return fmt.Errorf("asset AvailableUnits cannot be less then 0")
+	}
 
+	data, err := ctx.GetStub().GetState(asset.ISIN)
+	if err == nil || data != nil {
+		fmt.Errorf("asset already exists")
+		return fmt.Errorf("asset already exists")
+	}
 
+	assetInBytes, err := json.Marshal(asset)
+	if err != nil {
+		fmt.Errorf("error in mashalling asset %s", err.Error())
+		return err
+	}
 
+	err = ctx.GetStub().PutState(asset.ISIN, assetInBytes)
+	if err != nil {
+		fmt.Errorf("error in adding asset %s", err.Error())
+		return err
+	}
+
+	err = ctx.GetStub().SetEvent("RegisterAsset", assetInBytes)
+	if err != nil {
+		fmt.Errorf("error in setting event %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (s *AssetManagementContract) SubscribeAsset(ctx contractapi.TransactionContextInterface, requestJSON string) error {
+
+	var request SubscriptionRedeemRequest
+	err := json.Unmarshal([]byte(requestJSON), &request)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal request: %v", err)
+	}
+
+	assetJSON, err := ctx.GetStub().GetState(request.ISIN)
+	if err != nil {
+		return fmt.Errorf("failed to read asset from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return fmt.Errorf("asset with ISIN %s does not exist", request.ISIN)
+	}
+
+	investorJSON, err := ctx.GetStub().GetState(request.InvestorID)
+	if err != nil {
+		return fmt.Errorf("failed to read investor from world state: %v", err)
+	}
+	if investorJSON == nil {
+		return fmt.Errorf("investor with ID %s does not exist", request.InvestorID)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return err
+	}
+	var investor Investor
+	err = json.Unmarshal(investorJSON, &investor)
+	if err != nil {
+		return err
+	}
+
+	if investor.Holdings == nil {
+		investor.Holdings = make(map[string]int)
+	}
+	if investor.SubscriptionTimestamps == nil {
+		investor.SubscriptionTimestamps = make(map[string]int64)
+	}
+
+	if request.Units > asset.AvailableUnits {
+		return fmt.Errorf("requested units exceed available units for asset %s", request.ISIN)
+	}
+
+	totalCost := request.Units * asset.PricePerUnit
+	if totalCost > investor.Balance {
+		return fmt.Errorf("insufficient balance for investor %s", request.InvestorID)
+	}
+
+	asset.AvailableUnits -= request.Units
+
+	investor.Balance -= totalCost
+	investor.Holdings[request.ISIN] = request.Units
+	investor.SubscriptionTimestamps[request.ISIN] = request.Timestamp
+
+	assetJSON, err = json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+	err = ctx.GetStub().PutState(request.ISIN, assetJSON)
+	if err != nil {
+		return err
+	}
+
+	investorJSON, err = json.Marshal(investor)
+	if err != nil {
+		return err
+	}
+	err = ctx.GetStub().PutState(request.InvestorID, investorJSON)
+	if err != nil {
+		return err
+	}
+
+	eventPayload := fmt.Sprintf("Investor %s subscribed to %d units of asset %s", request.InvestorID, request.Units, request.ISIN)
+	return ctx.GetStub().SetEvent("SubscriptionEvent", []byte(eventPayload))
+}
+
+func (s *AssetManagementContract) RedeemAsset(ctx contractapi.TransactionContextInterface, requestJSON string) error {
+
+	var request SubscriptionRedeemRequest
+	err := json.Unmarshal([]byte(requestJSON), &request)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal request: %v", err)
+	}
+
+	assetJSON, err := ctx.GetStub().GetState(request.ISIN)
+	if err != nil {
+		return fmt.Errorf("failed to read asset from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return fmt.Errorf("asset with ISIN %s does not exist", request.ISIN)
+	}
+
+	investorJSON, err := ctx.GetStub().GetState(request.InvestorID)
+	if err != nil {
+		return fmt.Errorf("failed to read investor from world state: %v", err)
+	}
+	if investorJSON == nil {
+		return fmt.Errorf("investor with ID %s does not exist", request.InvestorID)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return err
+	}
+	var investor Investor
+	err = json.Unmarshal(investorJSON, &investor)
+	if err != nil {
+		return err
+	}
+
+	if investor.Holdings[request.ISIN] < request.Units {
+		return fmt.Errorf("insufficient holdings for investor %s", request.InvestorID)
+	}
+
+	if request.Units < 30 {
+		return fmt.Errorf("minimum redemption limit is 30 units")
+	}
+
+	lockInEndTime := investor.SubscriptionTimestamps[request.ISIN] + LockInTime
+	if request.Timestamp < lockInEndTime {
+		return fmt.Errorf("asset %s is still under lock-in period", request.ISIN)
+	}
+
+	asset.AvailableUnits += request.Units
+	investor.Balance += request.Units * asset.PricePerUnit
+	investor.Holdings[request.ISIN] = investor.Holdings[request.ISIN] - request.Units
+
+	assetJSON, err = json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+	err = ctx.GetStub().PutState(request.ISIN, assetJSON)
+	if err != nil {
+		return err
+	}
+
+	investorJSON, err = json.Marshal(investor)
+	if err != nil {
+		return err
+	}
+	err = ctx.GetStub().PutState(request.InvestorID, investorJSON)
+	if err != nil {
+		return err
+	}
+
+	eventPayload := fmt.Sprintf("Investor %s redeemed %d units of asset %s", request.InvestorID, request.Units, request.ISIN)
+	return ctx.GetStub().SetEvent("RedemptionEvent", []byte(eventPayload))
+}
+
+func (s *AssetManagementContract) GetPortfolio(ctx contractapi.TransactionContextInterface, investorID string) (map[string]interface{}, error) {
+	investorJSON, err := ctx.GetStub().GetState(investorID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read investor from world state: %v", err)
+	}
+	if investorJSON == nil {
+		return nil, fmt.Errorf("investor with ID %s does not exist", investorID)
+	}
+
+	var investor Investor
+	err = json.Unmarshal(investorJSON, &investor)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal investor: %v", err)
+	}
+
+	portfolio := make(map[string]int)
+	for isin, units := range investor.Holdings {
+		portfolio[isin] = units
+	}
+	result := map[string]interface{}{
+		"balance":   investor.Balance,
+		"portfolio": portfolio,
+	}
+
+	return result, nil
+}
 func main() {
 	chaincode, err := contractapi.NewChaincode(&AssetManagementContract{})
 	if err != nil {
@@ -53,4 +326,3 @@ func main() {
 		fmt.Printf("Error starting asset management chaincode: %v", err)
 	}
 }
-
--- client/client.go
+++ client/client.go
@@ -1,51 +1,281 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
-	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/core"
-	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"os"
+	"path"
+	"time"
+	"crypto/x509"
 )
 
-func main() {
-	// Set up fabric connection
-	wallet, err := gateway.NewFileSystemWallet("./wallet")
+const (
+	channelName   = "assetchannel"
+	chaincodeName = "assetcc"
+	mspID         = "Org1MSP"
+	cryptoPath    = "../../Documents/fabric/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
+	certPath      = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
+	keyPath       = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
+	tlsCertPath   = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	peerEndpoint  = "localhost:7051"
+	gatewayPeer   = "peer0.org1.example.com"
+)
+
+type Asset struct {
+	ISIN           string `json:"isin"`
+	CompanyName    string `json:"company_name"`
+	AssetType      string `json:"asset_type"`
+	TotalUnits     int    `json:"total_units"`
+	PricePerUnit   int    `json:"price_per_unit"`
+	AvailableUnits int    `json:"available_units"`
+}
+
+type Investor struct {
+	InvestorID             string           `json:"investor_id"`
+	Balance                int              `json:"balance"`
+	Holdings               map[string]int   `json:"holdings"`
+	SubscriptionTimestamps map[string]int64 `json:"subscriptionTimestamps"`
+}
+
+// TODO: Create a new investor
+
+func CreateInvestor(contract *client.Contract, investor Investor) error {
+
+	investorJSON, err := json.Marshal(investor)
 	if err != nil {
-		fmt.Println("Failed to create wallet: ", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to marshal the investor details: %v", err)
 	}
 
-	// Get gateway connection
-	gw, err := gateway.Connect(
-		gateway.WithAddress("localhost:7051"),
-		gateway.WithIdentity(wallet, "admin"),
+	_, err = contract.SubmitTransaction("CreateUser", string(investorJSON))
+	if err != nil {
+		return fmt.Errorf("failed to create the investor: %v", err)
+	}
+
+	fmt.Printf("successfully created the investor")
+	return nil
+
+}
+
+// TODO: Register a new asset
+
+func RegisterAsset(contract *client.Contract, asset Asset) error {
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return fmt.Errorf("failed to marshal asset details: %v", err)
+	}
+
+	_, err = contract.SubmitTransaction("RegisterAsset", string(assetJSON))
+	if err != nil {
+		return fmt.Errorf("failed to register the asset details: %v", err)
+	}
+
+	fmt.Printf("successfully registered the asset details")
+	return nil
+}
+
+// TODO: Subscribe to the asset
+
+func SubscribeAsset(contract *client.Contract, investorID string, isin string, units int) error {
+
+	_, err := contract.SubmitTransaction("SubscribeAsset", investorID, isin, fmt.Sprint("%d", units))
+	if err != nil {
+		return fmt.Errorf("failed to subscribe to the asset: %v", err)
+	}
+
+	fmt.Printf("successfully subscribed to the asset")
+	return nil
+}
+
+// TODO: Redeem assets
+
+func RedeemAsset(contract *client.Contract, investorID string, isin string, units int) error {
+
+	_, err := contract.SubmitTransaction("RedeemAsset", investorID, isin, fmt.Sprint("%d", units))
+	if err != nil {
+		return fmt.Errorf("failed to redeem asset details: %v", err)
+	}
+
+	fmt.Printf("successfully redeemed the asset: %s\n")
+	return nil
+}
+
+// TODO: Check the investor's portfolio balance
+
+func GetPortfolio(contract *client.Contract, investorID string) error {
+
+	result, err := contract.EvaluateTransaction("GetPortfolio", investorID)
+	if err != nil {
+		return fmt.Errorf("failed to get portfolio details: %v", err)
+	}
+
+	fmt.Printf("successfully retrieved the portfolio details %s\n", string(result))
+	return nil
+}
+
+// Listen to chaincode events
+
+func listenToEvents(eventChannel <-chan *client.ChaincodeEvent) {
+	// Register a subscription to chaincode events
+	fmt.Println("Listening to chaincode events...")
+
+	for {
+		select {
+		case ccEvent := <-eventChannel:
+			fmt.Printf("Received chaincode event: %s\n", ccEvent.Payload)
+		case <-time.After(10 * time.Second):
+			fmt.Println("Timeout: No events received for 10 seconds")
+			return
+		}
+	}
+}
+
+func main() {
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	id := newIdentity()
+	sign := newSign()
+
+	gateway, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
 	)
 	if err != nil {
-		fmt.Println("Failed to connect to gateway: ", err)
-		os.Exit(1)
+		panic(err)
 	}
+	defer gateway.Close()
 
-	// Create a client for interacting with the blockchain
-	channelClient, err := channel.New(gw, channel.WithChannelID("assetchannel"))
+	network := gateway.GetNetwork(channelName)
+	contract := network.GetContract(chaincodeName)
+	eventChannel, err := network.ChaincodeEvents(context.Background(), chaincodeName)
 	if err != nil {
-		fmt.Println("Failed to create channel client: ", err)
+		panic(err)
+	}
+
+	go listenToEvents(eventChannel)
+
+	// Create a new investor
+	investor := Investor{
+		InvestorID: "investor1",
+		Balance:    10000,
+	}
+	err = CreateInvestor(contract, investor)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// TODO: Create a new investor
-	
-	
-	// TODO: Register a new asset
-	
-	
-	// TODO: Subscribe to the asset
-	
-	
-	// TODO: Redeem assets
-	
-	
-	// TODO: Check the investor's portfolio balance
+	// Register a new asset
+	asset := Asset{
+		ISIN:           "US1234567890",
+		CompanyName:    "Example Corp",
+		AssetType:      "Stock",
+		TotalUnits:     1000,
+		PricePerUnit:   100,
+		AvailableUnits: 1000,
+	}
+	err = RegisterAsset(contract, asset)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	// Subscribe to an asset
+	err = SubscribeAsset(contract, "investor1", "US1234567890", 10)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	// Redeem asset units
+	err = RedeemAsset(contract, "investor1", "US1234567890", 5)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Get investor's portfolio
+	err = GetPortfolio(contract, "investor1")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+func newIdentity() *identity.X509Identity {
+	certificate, err := loadCertificate(certPath)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := identity.NewX509Identity(mspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func loadCertificate(filename string) (*x509.Certificate, error) {
+	certificatePEM, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read certificate file: %w", err)
+	}
+	return identity.CertificateFromPEM(certificatePEM)
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func newSign() identity.Sign {
+	files, err := os.ReadDir(keyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key directory: %w", err))
+	}
+	privateKeyPEM, err := os.ReadFile(path.Join(keyPath, files[0].Name()))
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
+
+// newGrpcConnection creates a gRPC connection to the Gateway server.
+func newGrpcConnection() *grpc.ClientConn {
+	certificate, err := loadCertificate(tlsCertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+
+	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	if err != nil {
+		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
+	}
+
+	return connection
 }
